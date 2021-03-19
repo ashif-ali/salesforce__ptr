f4c910a753885dd728fe8e2c60e6d7e0d881fc52
ashif-ali <ashifali2327@gmail.com>
2021-03-19 16:05:43 +0530

    added records and iterators

---
+++ force-app/main/default/lwc/exploreIterators/exploreIterators.js
@@ -0,0 +1,7 @@
+import { LightningElement, wire } from "lwc";
+import getRecords from "@salesforce/apex/ExploreIteratorController.getRecords";
+
+export default class ExploreIterators extends LightningElement {
+  @wire(getRecords)
+  records;
+}
